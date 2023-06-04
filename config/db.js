a5dadd40c7b674361446a18360738d5da424625d
Mushishi Dickson <64264319+kson99@users.noreply.github.com>
2023-06-04 21:38:27 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,22 @@
 const mysql = require("mysql");
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.HOST_KEY,
   user: process.env.USER_NAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
+});
+
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("Error connecting to MySQL database", err);
+    return;
+  }
+
+  console.log("Connected to MySQL database!");
+
+  //release connection
+  connection.release();
 });
 
 module.exports = db;
